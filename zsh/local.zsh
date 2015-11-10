05fe8a5810f36fd5c07b5900096cddb450d4e494
Piotr Witoslawski <pwitoslawski@opera.com>
2015-11-10 09:24:44 +0100

    Various improvements

--- zsh/local.zsh
+++ zsh/local.zsh
@@ -2,10 +2,11 @@ export LANG="en_US.UTF-8"
 export EDITOR=vim
 
 alias update="sudo emaint sync -a && sudo eix-update && sudo emerge --with-bdeps=y --keep-going -avDuN @world"
-alias tmux='TERM=screen-256color tmux -2'
+alias tmux='TERM=tmux-256color tmux -2'
 alias vim="NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim"
 alias ff="export MOZ_USE_OMTC=1 && nohup firefox &>/dev/null &"
-
+alias spotify="apulse spotify"
+eval `dircolors /home/pwitoslawski/.dir_colors`
 if which tmux 2>&1 >/dev/null; then
     # if no session is started, start a new session
     test -z ${TMUX} && tmux
